886122e65252cc576a0e8f03a52ec4286def19f3
emmanuelgaribay.dev@gmail.com <egaribaydev@DESKTOP-01Q0V8J.localdomain>
2026-05-25 20:54:41 -0600

    Nuevo commit Otros

--- JS/app.js
+++ JS/app.js
@@ -1,19 +1,12 @@
-
-
-
-
-
-
 //? OTROS
 
 document.addEventListener("DOMContentLoaded", function() {
     const elementoCarrusel = document.querySelector('#miCarrusel');
 
-  // Inicializamos el carrusel con JavaScript
+  
     const miCarrusel = new bootstrap.Carousel(elementoCarrusel, {
-    interval: 3000, // Tiempo en milisegundos para cambiar de imagen
-    wrap: true      // Vuelve a empezar al llegar al final
+    interval: 3000, 
+    wrap: true      
     });
 });
-
 
